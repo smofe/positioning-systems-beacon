9d2ad509543eb8c6cb0f176930c0efeb7a1b9287
Michel <michel.klappert@student.hpi.de>
2021-06-08 17:55:39 +0200

    patient beacon config

--- src/main.cpp
+++ src/main.cpp
@@ -51,13 +51,9 @@
 #include "BLEServer.h"
 #include "BLEBeacon.h"
 
-RTC_DATA_ATTR static time_t last;        // remember last boot in RTC Memory
-RTC_DATA_ATTR static uint32_t bootcount; // remember number of boots in RTC Memory
-
 // See the following for generating UUIDs:
 // https://www.uuidgenerator.net/
 BLEAdvertising *pAdvertising;   // BLE Advertisement type
-struct timeval now;
 
 #define BEACON_UUID "87b99b2c-90fd-11e9-bc42-526af7764f64" // UUID 1 128-Bit (may use linux tool uuidgen or random numbers via https://www.uuidgenerator.net/)
 
@@ -66,8 +62,8 @@ void setBeacon() {
   BLEBeacon oBeacon = BLEBeacon();
   oBeacon.setManufacturerId(0x4C00); // fake Apple 0x004C LSB (ENDIAN_CHANGE_U16!)
   oBeacon.setProximityUUID(BLEUUID(BEACON_UUID));
-  oBeacon.setMajor((bootcount & 0xFFFF0000) >> 16);
-  oBeacon.setMinor(bootcount & 0xFFFF);
+  oBeacon.setMajor(0);
+  oBeacon.setMinor(0);
   BLEAdvertisementData oAdvertisementData = BLEAdvertisementData();
   BLEAdvertisementData oScanResponseData = BLEAdvertisementData();
 
@@ -87,13 +83,9 @@ void setBeacon() {
 void setup() {
 
   Serial.begin(115200);
-  gettimeofday(&now, NULL);
-  Serial.printf("start ESP32 %d\n", bootcount++);
-  Serial.printf("deep sleep (%lds since last reset, %lds since last boot)\n", now.tv_sec, now.tv_sec - last);
-  last = now.tv_sec;
-
+ 
   // Create the BLE Device
-  BLEDevice::init("ESP32 as iBeacon");
+  BLEDevice::init("patient-4");
   BLEDevice::setPower(ESP_PWR_LVL_P9, ESP_BLE_PWR_TYPE_ADV);
   // Create the BLE Server
   BLEServer *pServer = BLEDevice::createServer(); // <-- no longer required to instantiate BLEServer, less flash and ram usage
@@ -103,11 +95,10 @@ void setup() {
    // Start advertising
   pAdvertising->start();
  
-  Serial.println("Advertizing started...");
+  Serial.println("Advertising started...");
   delay(100);
 }
 
 void loop() {
  
-  //pAdvertising->stop();
 }
